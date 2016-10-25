55927ad6c943347dd97fb80db80b2d228d68d243
worthingtont12 <worthingtont12@gmail.com>
2016-10-25 14:42:34 -0400

    moving folders

---
+++ Data_Collection/Query.py
@@ -0,0 +1,22 @@
+#import re
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client['Texas_tweets']
+collection = db['twitter_collection']
+tweets_iterator = collection.find()
+tweets = []
+for tweet in tweets_iterator:
+    tweets.append(tweet['id'])
+
+print(len(tweets))
+    #if tweet['lang'] == 'en':
+        #for i in tweets_iterator:
+        #tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet['text']).split())
+        #print(tweet['text'])
+#text = tweet['text']
+#user_screen_name = tweet['user']['screen_name']
+#user_name = tweet['user']['name']
+#retweet_count = tweet['retweeted_status']['retweet_count']
+#retweeted_name = tweet['retweeted_status']['user']['name']
+#retweeted_screen_name = tweet['retweeted_status']['user']['screen_name']
+#./mongoexport --host localhost --db Washington_tweets --collection twitter_collection --csv > Washington_tweets.csv --fields id,user.id,text,lang,user.lang,user.location,user.screen_name,coordinates
