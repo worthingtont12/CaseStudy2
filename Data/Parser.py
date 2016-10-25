bb1bbdf483e5b1cbb5022e81036ce6b366ff7740
worthingtont12 <worthingtont12@gmail.com>
2016-10-25 17:27:23 -0400

    parser updated

--- Data/Parser.py
+++ Data/Parser.py
@@ -1,7 +1,7 @@
 """Parse Tweets and export them into a working format for Topic Modeling."""
 
 import os
-#import re
+import re
 import pandas as pd
 os.chdir("/Users/tylerworthington/Git_Repos/CaseStudy2/Data/")
 
@@ -23,13 +23,27 @@ print(len(set(Tex['user.id'])))
 print(len(set(Wash['id'])))
 print(len(set(Wash['user.id'])))
 
-#collapsing tweets by user.id
-
 #stripping non text characters ie @, # ,https://, ect
-#for i in Texas['text']:
-#    tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", Texas['text']).split())
-#    print(Texas['text'])
+clean1 = []
+for i in Tex['text']:
+    tmp = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", i).split())
+    clean1.append(tmp)
+
+Tex['cleaned.text'] = clean1
+
+clean2 = []
+for i in Wash['text']:
+    tmp1 = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", i).split())
+    clean2.append(tmp1)
+
+Wash['cleaned.text'] = clean2
+
+#collapsing tweets by user.id
+Wash['author.text'] = Wash[['user.id', 'cleaned.text']].groupby(['user.id'])['cleaned.text'].transform(lambda x: ','.join(x))
+Tex['author.text'] = Tex[['user.id', 'cleaned.text']].groupby(['user.id'])['cleaned.text'].transform(lambda x: ','.join(x))
+
+print(Wash['author.text'])
 
 #Export Tweets
-#Tex.to_csv('Texas_cleaned.csv')
-#Wash.to_csv('Washington_cleaned.csv')
+Tex.to_csv('Texas_cleaned.csv')
+Wash.to_csv('Washington_cleaned.csv')
