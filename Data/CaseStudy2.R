4ac67340466d4b94237319966a3924fc9ac47c11
igy0309 <yy3fs@virginia.edu>
2016-10-25 17:28:37 -0400

    Update CaseStudy2.R

--- Data/CaseStudy2.R
+++ Data/CaseStudy2.R
@@ -14,3 +14,24 @@ texas.other <- texas[! (grepl('austin', texas$user.location, ignore.case = TRUE)
                           grepl('houston', texas$user.location, ignore.case = TRUE)) & 
                        (grepl('TX', texas$user.location) | 
                           grepl('texas', texas$user.location, ignore.case = TRUE)), ]
+
+
+
+library(XML)
+library(tm)
+library(topicmodels)
+
+# clean and compute tfidf
+
+seattle.text <- VCorpus(DataframeSource(as.data.frame(seattle$text)))
+seattle.text.clean = tm_map(seattle.text, stripWhitespace)                          # remove extra whitespace
+seattle.text.clean = tm_map(seattle.text.clean, removeNumbers)                      # remove numbers
+seattle.text.clean = tm_map(seattle.text.clean, removePunctuation)                  # remove punctuation
+seattle.text.clean = tm_map(seattle.text.clean, content_transformer(tolower))       # ignore case
+seattle.text.clean = tm_map(seattle.text.clean, removeWords, stopwords("english"))  # remove stop words
+
+seattle.text.clean[[2]]$content
+
+seattle.text.clean = tm_map(seattle.text.clean, stemDocument)                       # stem all words
+seattle.text.clean.tf = DocumentTermMatrix(seattle.text.clean, control = list(weighting = weightTf))
+
