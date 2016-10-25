ef13e360c595a349bfb7314fb9deb708f7d0ccbe
igy0309 <yy3fs@virginia.edu>
2016-10-25 18:10:57 -0400

    Update CaseStudy2.R

--- Data/CaseStudy2.R
+++ Data/CaseStudy2.R
@@ -35,14 +35,53 @@ library(XML)
 library(tm)
 library(topicmodels)
 
-# clean and compute tfidf
-
-seattle.text <- VCorpus(DataframeSource(as.data.frame(seattle$text)))
+# clean Seattle data
+seattle.text <- VCorpus(DataframeSource(as.data.frame(seattle$author.text)))
 seattle.text.clean = tm_map(seattle.text, stripWhitespace)                          # remove extra whitespace
 seattle.text.clean = tm_map(seattle.text.clean, removeNumbers)                      # remove numbers
 seattle.text.clean = tm_map(seattle.text.clean, removePunctuation)                  # remove punctuation
 seattle.text.clean = tm_map(seattle.text.clean, content_transformer(tolower))       # ignore case
 seattle.text.clean = tm_map(seattle.text.clean, removeWords, stopwords("english"))  # remove stop words
+
+# clean Washington.Other data
+washington.other.text <- VCorpus(DataframeSource(as.data.frame(washington.other$author.text)))
+washington.other.text.clean = tm_map(washington.other.text, stripWhitespace)                          # remove extra whitespace
+washington.other.text.clean = tm_map(washington.other.text.clean, removeNumbers)                      # remove numbers
+washington.other.text.clean = tm_map(washington.other.text.clean, removePunctuation)                  # remove punctuation
+washington.other.text.clean = tm_map(washington.other.text.clean, content_transformer(tolower))       # ignore case
+washington.other.text.clean = tm_map(washington.other.text.clean, removeWords, stopwords("english"))  # remove stop words
+
+# clean Houston data
+houston.text <- VCorpus(DataframeSource(as.data.frame(houston$author.text)))
+houston.text.clean = tm_map(houston.text, stripWhitespace)                          # remove extra whitespace
+houston.text.clean = tm_map(houston.text.clean, removeNumbers)                      # remove numbers
+houston.text.clean = tm_map(houston.text.clean, removePunctuation)                  # remove punctuation
+houston.text.clean = tm_map(houston.text.clean, content_transformer(tolower))       # ignore case
+houston.text.clean = tm_map(houston.text.clean, removeWords, stopwords("english"))  # remove stop words
+
+# clean Austin data
+austin.text <- VCorpus(DataframeSource(as.data.frame(austin$author.text)))
+austin.text.clean = tm_map(austin.text, stripWhitespace)                          # remove extra whitespace
+austin.text.clean = tm_map(austin.text.clean, removeNumbers)                      # remove numbers
+austin.text.clean = tm_map(austin.text.clean, removePunctuation)                  # remove punctuation
+austin.text.clean = tm_map(austin.text.clean, content_transformer(tolower))       # ignore case
+austin.text.clean = tm_map(austin.text.clean, removeWords, stopwords("english"))  # remove stop words
+
+# clean Dallas data
+dallas.text <- VCorpus(DataframeSource(as.data.frame(dallas$author.text)))
+dallas.text.clean = tm_map(dallas.text, stripWhitespace)                          # remove extra whitespace
+dallas.text.clean = tm_map(dallas.text.clean, removeNumbers)                      # remove numbers
+dallas.text.clean = tm_map(dallas.text.clean, removePunctuation)                  # remove punctuation
+dallas.text.clean = tm_map(dallas.text.clean, content_transformer(tolower))       # ignore case
+dallas.text.clean = tm_map(dallas.text.clean, removeWords, stopwords("english"))  # remove stop words
+
+# clean Texas.Other data
+texas.other.text <- VCorpus(DataframeSource(as.data.frame(texas.other$author.text)))
+texas.other.text.clean = tm_map(texas.other.text, stripWhitespace)                          # remove extra whitespace
+texas.other.text.clean = tm_map(texas.other.text.clean, removeNumbers)                      # remove numbers
+texas.other.text.clean = tm_map(texas.other.text.clean, removePunctuation)                  # remove punctuation
+texas.other.text.clean = tm_map(texas.other.text.clean, content_transformer(tolower))       # ignore case
+texas.other.text.clean = tm_map(texas.other.text.clean, removeWords, stopwords("english"))  # remove stop words
 
 seattle.text.clean[[2]]$content
 
