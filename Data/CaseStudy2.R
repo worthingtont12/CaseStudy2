55ab94e2b9b0ad2f26b2ae523cf2b9ee4a283ea3
igy0309 <yy3fs@virginia.edu>
2016-10-25 16:32:53 -0400

    Update CaseStudy2.R

--- Data/CaseStudy2.R
+++ Data/CaseStudy2.R
@@ -2,15 +2,15 @@ washington <- read.csv('Washington_tweets.csv')
 texas <- read.csv('Texas_tweets.csv')
 
 seattle <- washington[grepl('seattle', washington$user.location, ignore.case = TRUE),]
-washington.other <- washington[!grepl('eattle', washington$user.location) & 
+washington.other <- washington[!grepl('seattle', washington$user.location, ignore.case = TRUE) & 
                              (grepl('WA', washington$user.location) | 
-                                grepl('ashington', washington$user.location)), ]
+                                grepl('washington', washington$user.location, ignore.case = TRUE)), ]
 
 austin <- texas[grepl('austin', texas$user.location, ignore.case = TRUE),]
 dallas <- texas[grepl('dallas', texas$user.location, ignore.case = TRUE),]
 houston <- texas[grepl('houston', texas$user.location, ignore.case = TRUE),]
 texas.other <- texas[! (grepl('austin', texas$user.location, ignore.case = TRUE) | 
                           grepl('dallas', texas$user.location, ignore.case = TRUE) | 
-                          grepl('ouston', texas$user.location, ignore.case = TRUE)) & 
+                          grepl('houston', texas$user.location, ignore.case = TRUE)) & 
                        (grepl('TX', texas$user.location) | 
                           grepl('texas', texas$user.location, ignore.case = TRUE)), ]
